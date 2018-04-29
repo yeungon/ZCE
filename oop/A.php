3658a2c2613c5adc0f7b8032607f4370688c7338
yeungon <yeungon@gmail.com>
2018-04-29 01:32:24 -0600

    ví dụ một class

---
+++ oop/A.php
@@ -0,0 +1,19 @@
+<?php
+
+//ví dụ một class
+
+class A {
+    protected
+
+    public function __construct(){
+
+    }
+
+    protected function firstMethod(){
+
+    }
+
+    protected function secondMethod(){
+
+    }
+}
