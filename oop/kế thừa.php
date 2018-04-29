8b591fa856c341eea1a47eba527ac36455e52153
yeungon <yeungon@gmail.com>
2018-04-29 01:23:19 -0600

    test

---
+++ oop/kế thừa.php
@@ -0,0 +1,25 @@
+<?php
+class A {
+    private function whoami() {
+        echo 'A', PHP_EOL;
+    }
+
+    public function out() {
+        $this->whoami();
+    }
+}
+
+class B extends A {
+    private function whoami() {
+        echo 'B', PHP_EOL;
+    }
+
+    public function out() {
+        $this->whoami();
+    }
+}
+
+$a = new A;
+$a->out(); //A
+$b = new B;
+$b->out(); //B
