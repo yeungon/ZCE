9f9f8aa1543ba1545c39e6270445582cb13c157e
yeungon <yeungon@gmail.com>
2018-04-30 00:34:56 -0600

    fucntion return type. Trên php 7 chấp nhận tất cả các loại return type, kể cả NULL.

---
+++ 2)functions/function_return_types_php7.php
@@ -0,0 +1,16 @@
+<?php
+
+/*$a = new ArrayObject();
+
+print_r($a);
+*/
+function vidu (): array{
+	
+	return "ví dụ";
+}
+
+vidu(); 
+
+//Fatal error: Uncaught TypeError: Return value of vidu() must be of the type array, integer returned in C:\xampp\htdocs\www\ZCE
+
+//http://talks.php.net/taiwan15#/php7ret
