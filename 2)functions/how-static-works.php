00d1149d2982b8527576f0688cc18f0534d47eb9
yeungon <yeungon@gmail.com>
2018-05-01 01:50:27 -0600

    static in a function ==> keep the value after the function is called.

---
+++ 2)functions/how-static-works.php
@@ -0,0 +1,34 @@
+<?php
+	
+/*
+
+// quan trọng
+
+
+Special thanks to David Stockton. He explained really well how
+static works. If you don't believe in me, watch his video:
+
+https://www.youtube.com/watch?v=hbbdyyN9MRU
+
+Thanks David!
+*/
+
+function callMe() {
+	static $foo = 0; 
+	// Here is the trick. Static only define the value to $foo on the 
+	// fist call. After that, it skips it and assume the last value
+	// that was assigned to $foo.
+	
+	// Without static, it will assign 0 every time it's called
+	// and will return: 1 1 1 1 1 1 1 1 1 1
+	
+	return ++$foo;
+}
+
+for ($i = 10; $i < 20; $i++) {
+	echo callMe(). ' ';
+}
+
+// Outputs: 1 2 3 4 5 6 7 8 9 10
+
+echo PHP_EOL;
