a7c25864ce135aa9129d4e5dbea033d0099dee49
yeungon <yeungon@gmail.com>
2018-05-01 01:53:16 -0600

    static variable in function will keep the value after the function is called.

--- 2)functions/how-static-works.php
+++ 2)functions/how-static-works.php
@@ -32,3 +32,20 @@ for ($i = 10; $i < 20; $i++) {
 // Outputs: 1 2 3 4 5 6 7 8 9 10
 
 echo PHP_EOL;
+
+//ví dụ 2 về static
+
+
+function tract(){
+    
+    static $count = 0; //giá trị của biến static được bảo lưu sau mỗi lần chạy hàm. vãi lúa
+    $count++;
+    echo $count;
+    
+}
+
+tract(); //1
+tract(); //2
+tract(); //3
+
+//https://3v4l.org/iO4Ug
