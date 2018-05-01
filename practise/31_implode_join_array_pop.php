591fd96ea7f891f4e09f4a9d7ba0b52a47a0ddc0
yeungon <yeungon@gmail.com>
2018-05-01 16:20:15 -0600

    implode alias join

---
+++ practise/31_implode_join_array_pop.php
@@ -0,0 +1,10 @@
+<?php
+
+$array = array(
+   'red' => 'APPLE',
+   'yellow' => 'BANANA',
+   'orange' => 'ORANGE'
+  );
+count($array)<0?:count($array)<0?:array_pop($array);
+
+echo strlen(join("", $array));
