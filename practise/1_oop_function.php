f781c809d9b900bbfdf56a454450aad869f240c8
yeungon <yeungon@gmail.com>
2018-05-01 02:53:38 -0600

    bài tập 1 về oop và array, kết hợp với toán tử ** (mũ) trên php7 - push lên git,

---
+++ practise/1_oop_function.php
@@ -0,0 +1,21 @@
+<?php
+
+class ViDu
+{
+
+    public $jar;
+
+    public function get(int $key) : ?array
+    {
+        return [$this->jar => $key**3];
+    }
+
+}
+
+$abc = new ViDu;
+
+$abc->jar = "key";
+
+echo array_values($abc->get(2))[0];
+
+//kết quả https://3v4l.org/LC0VR
