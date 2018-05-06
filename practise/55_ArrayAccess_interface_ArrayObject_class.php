60b3b60b1f12ee1e7649b7c39fbaf7ae6c6f081b
Vuong Nguyen <yeungon@gmail.com>
2018-05-06 06:03:57 -0600

    Update 55_ArrayAccess_interface_ArrayObject_class.php
    arrayobject vs arrayaccess

--- practise/55_ArrayAccess_interface_ArrayObject_class.php
+++ practise/55_ArrayAccess_interface_ArrayObject_class.php
@@ -1,3 +1,7 @@
 <?php
-
+/**
+* ZCE
+* SPL: arrayobject and arrayaccess
+* question compiled by me@vuongnguyen.net
+*/
 echo in_array("ArrayAccess", get_declared_interfaces()) + in_array("ArrayObject", get_declared_classes());
