f5c4b59cf1bc0e56626f8c8449587fd7ed1d3cd3
yeungon <yeungon@gmail.com>
2018-05-07 03:03:40 -0600

    sort() sắp xếp số trước chữ, chữ hoa trước chữ thường.

--- practise/56_explode_sort_string.php
+++ practise/56_explode_sort_string.php
@@ -1,6 +1,6 @@
 <?php 
 
-$s = "Day la mot vi du thu vi dung khong ?"; 
-$r = explode(' ', ucfirst($s)); 
-sort($r); 
-echo implode(',', $r);
+$string = "Day la mot vi du thu vi dung khong ?"; 
+$array = explode(' ', ucfirst($string)); //ucfirst() chỉ echo chữ hoa của cả dãy string ucword() in chữ hoa của tất cả các chữ trong string
+sort($array); //sort () sắp xếp kí tự lạ trước => số => chữ hoa -> chữ thường abc
+echo implode(',', $array);
