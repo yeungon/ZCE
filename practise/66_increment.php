4d87da8aa7466d3cae4ebf2293f9f2557a6112d7
yeungon <yeungon@gmail.com>
2018-05-10 21:07:09 -0600

    increment

---
+++ practise/66_increment.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+*@author by Vuong
+*@category ZCE66 Increment
+*/
+
+$a = 1;
+
+$b = $a++; //tăng lên 2
+
+$a = $a + 1; //lên 3
+
+echo --$a; // còn 2 nếu pre-increment/decrement : CHAGNE -> USE
+
+
+
+//https://3v4l.org/RcD4J
