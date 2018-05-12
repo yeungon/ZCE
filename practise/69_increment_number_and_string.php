07ec97c2e284717a0e803d1e335e3001aad323b4
yeungon <yeungon@gmail.com>
2018-05-12 00:09:06 -0600

    string chỉ có tiến, không có lùi || với string "a9" => tăng thành "b0"

---
+++ practise/69_increment_number_and_string.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*@author by Vuong
+*@category ZCE69 Increment string
+*/
+
+$a = 'a9';
+$b = "b0";
+
+++$a; //nếu tăng 1 thì a9 --> b0
+
+--$b; //với string thì không có lùi
+
+echo $a[1], $b[1];
