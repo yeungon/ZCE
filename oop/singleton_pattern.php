97358d4677f345e04f37c7b58b7971961a109032
yeungon <yeungon@gmail.com>
2018-05-13 01:57:57 -0600

    singleton design pattern

---
+++ oop/singleton_pattern.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*@author by Vuong
+*@category Singelton Pattern
+*/
+
+class A{
+
+	//private chỉ có thể truy cập trong class || static: có thể gọi tới mà không cần khởi tạo
+	private static $instance; 
+
+	//thiết lập private để không cho khởi tạo object
+	private function __construct(){ 
+
+	}
+
+	//dùng phương thức static để gọi phương thức bằng :: mà không cần khởi tạo object
+	public static function getInstance(){ 
+		if (!isset(self::$instance)) //kiểm tra nếu chưa có
+			self::$instance = new A; //khởi tạo object
+			return self::$instance; //trả về object
+	}
+
+	//tạo object chính nó bằng cách gọi hàm/luôn luôn chỉ có một object giống nhau khi gọi hàm getInstance
+	$a = A::getInstance(); 
+}
