adf19d5c25400a55e9f465a3a2c96cbb141dedc6
yeungon <yeungon@gmail.com>
2018-05-14 00:50:52 -0600

    dependecy injection

---
+++ oop/dependency_injection_design_pattern.php
@@ -0,0 +1,52 @@
+<?php
+/**
+*@author by Vuong
+*@category Dependency Injection
+*/
+
+
+interface TheThao
+{
+    function dacdiem($message);
+}
+
+class BongDa implements TheThao
+{
+
+    public function dacdiem($message)
+    {
+        printf("Đây là thông tin Bóng Đá. Anh là %s \n", $message);
+    }
+}
+
+class BongChuyen implements TheThao 
+{
+
+    public function dacdiem($message) 
+    {
+        printf("Đây là thông tin Bóng chuyền. Anh là %s \n", $message);
+    }
+}
+
+class VanDongVien 
+{
+    public $vandongvien;
+
+    public function __construct(TheThao $vandongvien) 
+    {
+        $this->vandongvien = $vandongvien;
+    }
+
+    public function dacdiem($message)
+    {
+        return $this->vandongvien->dacdiem($message);
+    }
+}
+
+//dùng với class Bóng Đá
+$vuong = new VanDongVien(new BongDa);
+$vuong->dacdiem('cầu thủ bóng đá');
+
+// dùng với class bóng chuyền
+$vidu2 = new VanDongVien(new BongChuyen);
+$vidu2->dacdiem('cầu thủ bóng chuyền');
