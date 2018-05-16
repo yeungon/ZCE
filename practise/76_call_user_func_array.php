51432068c0aabd92c71ca7409b423ed92ed346ec
yeungon <yeungon@gmail.com>
2018-05-16 00:53:27 -0600

    call_user_func_array

---
+++ practise/76_call_user_func_array.php
@@ -0,0 +1,14 @@
+<?php
+
+class ViDu
+{
+    public $variable = 2;
+    public function hello($argument1, $argument2)
+    {
+        return $argument1**$argument2*$this->variable;
+    }
+}
+
+$a = new ViDu;
+
+echo call_user_func_array([$a, "hello"], [2, 3]);
