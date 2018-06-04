ac60dc5b005a905bc90350050393b7c23fa45fc0
yeungon <yeungon@gmail.com>
2018-06-03 23:51:04 -0600

    regex practice

---
+++ practise/87_regex.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* @category ZCE 86 REGEX
+* @author Vuong Nguyen me@vuongnguyen.net
+* @link https://3v4l.org/XS24H
+*/
+
+$string = "hello world";
+
+echo preg_match("/abc\d/", "abc4dfasfdsfadfs") <=> strpos($string, $string[2]);
