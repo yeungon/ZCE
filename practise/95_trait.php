8822c797a885490fd4c4753b85d70ba393bd84d8
Vuong Nguyen <yeungon@gmail.com>
2018-08-26 02:24:33 -0600

    trait cannot have CONSTANT

---
+++ practise/95_trait.php
@@ -0,0 +1,35 @@
+<?php
+
+/*by vuongnguyen*/
+/*
+
+trait không dùng hằng được.
+property của trait không thể được khai báo lại.
+
+*/
+trait vidu{
+    
+    public static $hello = ["false" => 1, "true" => 0];
+        
+}
+
+trait cuoituan{
+    
+    public function xinchao(array $a){
+        
+        return array_flip($a);
+    }
+
+}
+
+class abc{
+    
+    use vidu, cuoituan;
+    
+}
+
+$a = new abc;
+
+echo gettype($a->xinchao($a::$hello)[1]) ?? "xin chào";
+
+//https://3v4l.org/WKe0R
