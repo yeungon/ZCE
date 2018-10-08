c1357ab0af1dd96482d38f9ddaec7c1a8dbdc0db
Vuong Nguyen <yeungon@gmail.com>
2018-10-08 00:24:30 -0600

    bitwise

---
+++ practise/117_bitwsite_left.php
@@ -0,0 +1,18 @@
+<?php
+
+/*https://3v4l.org/P01gt
+Author: http://php-geeks-wink.blogspot.com/search/label/zce-zend-certified-engineer-php-basic-mock-test-answers
+
+*/
+    
+   @$a  =  10;
+   ${'b'}  =  20;
+   $c  =  4;
+   $d  =  8;
+   $e  =  1.0;
+   $f  =  $c  + $d * 2;
+   $g  =  $f  % 20;
+   $h  =  ${'b'}  - $a + $c + 2;
+   $i  =  $h  << $c;
+   $j  =  $i  * $e;
+   print $j;
