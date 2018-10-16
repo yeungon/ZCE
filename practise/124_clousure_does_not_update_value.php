2b1a0f1526786ee6c51be3c88e6b309594108586
Vuong Nguyen <yeungon@gmail.com>
2018-10-16 01:08:15 -0600

    clousure should be called by reference then the value of variable can be automatically called.

--- practise/124_clousure_does_not_update_value.php
+++ practise/124_clousure_does_not_update_value.php
@@ -3,11 +3,13 @@
 	https://3v4l.org/spvB5
 
 	Note: $name trong closure chỉ sử dụng giá trị được khai báo TRƯỚC closure/lambda
+
+	Nếu muốn dùng $name sau khi đã khai báo closure, dùng tham chiếu (reference - &$name) thay vì tham trị (value) khi gọi biến $name;
 */
 
 $name = [1 => "hello world", true => "xin chao", false => "gai dep", "me"];
 
-$f = function($argument) use ($name){
+$f = function($argument) use ($name){ // &$name if you want to update the value of variable automatically
     
     return count($name) ?? $argument*2; //$name sẽ được tuyền vào với giá trị được khai báo trước f()
 };
